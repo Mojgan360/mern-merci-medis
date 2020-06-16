749b67785b9ed760e11c375f3e8e369866a66e99
Mojgan360 <mojgan360@gmail.com>
2020-06-16 16:39:42 +0200

    Added structures

--- template.js
+++ template.js
@@ -1,13 +1,13 @@
 export default () => {
-    return `<!doctype html>
+  return `<!doctype html>
       <html lang="en">
         <head>
           <meta charset="utf-8">
-          <title>MERN Kickstart</title>
+          <title>Merci</title>
         </head>
         <body>
           <div id="root"></div>
           <script type="text/javascript" src="/dist/bundle.js"></script>
         </body>
-      </html>`
-}
+      </html>`;
+};
