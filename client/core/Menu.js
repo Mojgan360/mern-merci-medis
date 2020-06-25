48055ad02c6ffb1fd772d58a62c607b00e84826e
Mojgan360 <mojgan360@gmail.com>
2020-06-25 14:35:17 +0200

    Completed Menu.js

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -1,45 +1,65 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import HomeIcon from "@material-ui/icons/Home";
+import Button from "@material-ui/core/Button";
+import auth from "./../auth/auth-helper";
+import { Link, withRouter } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-export default function ButtonAppBar() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
+const isActive = (history, path) => {
+  if (history.location.pathname == path) return { color: "#ff4081" };
+  else return { color: "#ffffff" };
+};
+const Menu = withRouter(({ history }) => (
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6" color="inherit">
+        MERN Skeleton
+      </Typography>
+      <Link to="/">
+        <IconButton aria-label="Home" style={isActive(history, "/")}>
+          <HomeIcon />
+        </IconButton>
+      </Link>
+      <Link to="/users">
+        <Button style={isActive(history, "/users")}>Users</Button>
+      </Link>
+      {!auth.isAuthenticated() && (
+        <span>
+          <Link to="/signup">
+            <Button style={isActive(history, "/signup")}>Sign up</Button>
+          </Link>
+          <Link to="/signin">
+            <Button style={isActive(history, "/signin")}>Sign In</Button>
+          </Link>
+        </span>
+      )}
+      {auth.isAuthenticated() && (
+        <span>
+          <Link to={"/user/" + auth.isAuthenticated().user._id}>
+            <Button
+              style={isActive(
+                history,
+                "/user/" + auth.isAuthenticated().user._id
+              )}
+            >
+              My Profile
+            </Button>
+          </Link>
+          <Button
             color="inherit"
-            aria-label="menu"
+            onClick={() => {
+              auth.clearJWT(() => history.push("/"));
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-}
+            Sign out
+          </Button>
+        </span>
+      )}
+    </Toolbar>
+  </AppBar>
+));
+
+export default Menu;
