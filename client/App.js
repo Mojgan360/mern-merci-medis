d19ea0e449de48ede911ccebb8d41b94b5779674
Mojgan360 <mojgan360@gmail.com>
2020-06-25 14:12:35 +0200

    Added api-user

--- client/App.js
+++ client/App.js
@@ -6,6 +6,12 @@ import { ThemeProvider } from "@material-ui/styles";
 import theme from "./theme";
 
 const App = () => {
-  return <h1>hello word</h1>;
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <MainRouter />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 };
 export default hot(module)(App);
--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
+import Menu from "./core/Menu";
+
 const MainRouter = () => {
   return (
     <div>
+      <Menu />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
