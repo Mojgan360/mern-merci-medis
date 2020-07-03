e9463e344f5f0a216b2573780e25d0c7f61cee22
Mojgan360 <mojgan360@gmail.com>
2020-07-03 22:04:23 +0200

    Assed post

---
+++ client/Menu.js
@@ -0,0 +1,85 @@
+import React from "react";
+//material-uo
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import HomeIcon from "@material-ui/icons/Home";
+import Button from "@material-ui/core/Button";
+//
+import auth from "./../auth/auth-helper";
+import { Link, withRouter } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#392F5A",
+  },
+}));
+
+const isActive = (history, path) => {
+  if (history.location.pathname == path) return { color: "#F092DD" };
+  else return { color: "#ffffff" };
+};
+
+//The withRouter HOC gives us direct access to a history prop.
+const Menu = withRouter(({ history }) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar className={classes.root} position="static" className={classes.root}>
+      <Toolbar>
+        <Typography variant="h6">MERCI</Typography>
+        <Link to="/" className={classes.root}>
+          <IconButton aria-label="Home" style={isActive(history, "/")}>
+            <HomeIcon />
+          </IconButton>
+        </Link>
+        <Link style={{ textDecoration: "none", color: "white" }} to="/users">
+          <Button style={isActive(history, "/users")}>Users</Button>
+        </Link>
+        {!auth.isAuthenticated() && (
+          <span component="div">
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/signup"
+            >
+              <Button style={isActive(history, "/signup")}>Sign up</Button>
+            </Link>
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/signin"
+            >
+              <Button style={isActive(history, "/signin")}>Sign In</Button>
+            </Link>
+          </span>
+        )}
+        {auth.isAuthenticated() && (
+          <span component="div">
+            <Link to={"/user/" + auth.isAuthenticated().user._id}>
+              <Button
+                style={isActive(
+                  history,
+                  "/user/" + auth.isAuthenticated().user._id
+                )}
+              >
+                My Profile
+              </Button>
+            </Link>
+            <Button
+              color="inherit"
+              onClick={() => {
+                auth.clearJWT(() => history.push("/"));
+              }}
+            >
+              Sign out
+            </Button>
+          </span>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+});
+
+export default Menu;
--- client/auth/api-auth.js
+++ client/auth/api-auth.js
@@ -1,64 +1,30 @@
-//Sign-in
 const signin = async (user) => {
   try {
-    let response = await fetch("/auth/signin/", {
-      method: "POST",
+    let response = await fetch('/auth/signin/', {
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       },
-      credentials: "include",
-      body: JSON.stringify(user),
-    });
-    return await response.json();
-  } catch (err) {
-    console.log(err);
+      credentials: 'include',
+      body: JSON.stringify(user)
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
   }
-};
+}
 
-//read
-const read = async (params, credentials, signal) => {
-  try {
-    let response = await fetch("/api/users/" + params.userId, {
-      method: "GET",
-      signal: signal,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + credentials.t,
-      },
-    });
-    return await response.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
-//update
-const update = async (params, credentials, user) => {
-  try {
-    let response = await fetch("/api/users/" + params.userId, {
-      method: "PUT",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + credentials.t,
-      },
-      body: JSON.stringify(user),
-    });
-    return await response.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
-//Sign-out
 const signout = async () => {
   try {
-    let response = await fetch("/auth/signout/", { method: "GET" });
-    return await response.json();
-  } catch (err) {
-    console.log(err);
+    let response = await fetch('/auth/signout/', { method: 'GET' })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
   }
-};
+}
 
-//
-export { signin, signout, update, read };
+export {
+  signin,
+  signout
+}
