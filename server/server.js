7b8812196c77f4373029d7e29c8c4f44df8bc060
Mojgan360 <mojgan360@gmail.com>
2020-07-07 13:01:29 +0200

    change production

--- server/server.js
+++ server/server.js
@@ -13,15 +13,15 @@ mongoose.connect(config.mongoUri, {
 mongoose.connection.on("error", () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`);
 });
-//Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// //Serve static assets in production
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err);
